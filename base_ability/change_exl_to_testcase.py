b7d6635ad3e1ead80d4bb431ea9b8767d36b03f7
v_zhjpzheng <v_zhjpzheng>
2022-11-05 20:09:00 +0800

    base_login

--- base_ability/change_exl_to_testcase.py
+++ base_ability/change_exl_to_testcase.py
@@ -1,23 +1,31 @@
-
 # coding = utf-8
 import pandas
 from base_ability import *
 
+
 class Interface_job(Base_interface):
-    def get_excel_data(self,filepath,sheet_name):
-        df = pandas.read_excel(filepath,sheet_name=sheet_name)
+    def get_excel_data(self, filepath, sheet_name):
+        df = pandas.read_excel(filepath, sheet_name=sheet_name)
         data = df.values
-        # print(data[0])
+        case = []
         for line in data:
-            c = str(line)
-            print(c)
-            # c = c.replace('[','')
-            # c = c.replace(']','')
-            # c = c.replace("'","")
-            # linelist = c.split(' ')
-            # print(linelist)
-                # with open('12222.py','w') as sd:
-                #     sd.write(st.range('A%i' % row).value)
-                #     sd.close()
+            data = {
+                "用例编号": line[0],
+                "用例标题": line[1],
+                "请求接口类别": line[2],
+                "请求地址":line[3],
+                "输入数据":line[4],
+                "数据格式": line[5],
+                "请求方式": line[6],
+                "是否需要登录": line[7],
+                "期望结果": line[8],
+            }
+            for key, value in data.items():
+                if str(value) == "nan":
+                    data['%s' % key] = None
+            case.append(case)
+        return case
+
+
 b = Interface_job()
-b.get_excel_data("base.xlsx","Sheet1")
+b.get_excel_data("base.xlsx", "Sheet1")
